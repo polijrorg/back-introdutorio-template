5227ad571c7f77efbccdb5d85f5d871609d66880
fefaortiz <fernanda.ortiz@polijunior.com.br>
2024-02-04 16:21:00 -0300

    Revert "save_CRUD_user"
    This reverts commit 485fb1eb164349c81553175eca0e4a7ceb871257.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,8 @@
 import { application, Router } from 'express';
 import testsRouter from './tests.routes';
-import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/tests', testsRouter);
-routes.use('/user', usersRouter);
-
 
 export default routes;
